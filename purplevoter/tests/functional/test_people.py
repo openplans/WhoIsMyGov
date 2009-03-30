0f495ec40f22333ab545bb1b59b88a15597e4933
Nicholas Bergson-Shilcock http://nicholasbs.net <devnull@localhost>
2009-03-30 15:29:26 -0500

    hacky functional test for geocoding (should really be split into several unit tests...)

--- purplevoter/tests/functional/test_people.py
+++ purplevoter/tests/functional/test_people.py
@@ -5,3 +5,21 @@ class TestPeopleController(TestController):
     def test_index(self):
         response = self.app.get(url(controller='people', action='index'))
         # Test response...
+
+    def test_geocoder(self):
+        response = self.app.get(url(controller='people', action='search', address='669 degraw st, 11217'))
+        # senate
+        assert 'Charles Schumer' in response
+        assert 'Kirsten Gillibrand' in response
+
+        # fed rep - D 11
+        assert 'Yvette D. Clarke' in response
+
+        # city council
+        assert 'David Yassky' in response
+
+        # state assembly - D 52
+        assert 'Joan Millman' in response
+
+        # state senate - D 18
+        assert 'Velmanette Montgomery' in response
