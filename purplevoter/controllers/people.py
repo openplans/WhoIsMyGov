23b95945acd4e84acfea14a53bc11e00678f127a
Nicholas Bergson-Shilcock http://nicholasbs.net <devnull@localhost>
2009-03-30 14:46:51 -0500

    first pass at adding geocoding

--- purplevoter/controllers/people.py
+++ purplevoter/controllers/people.py
@@ -1,9 +1,12 @@
 import logging
 
 from pylons import request, response, session, tmpl_context as c
+from pylons import config
 from pylons.controllers.util import abort, redirect_to
 
 from purplevoter.lib.base import BaseController, render
+
+import geopy
 
 log = logging.getLogger(__name__)
 
@@ -16,4 +19,24 @@ class PeopleController(BaseController):
         return 'Hello World'
 
     def search(self):
+        if request.params.has_key('address'):
+            lat, lon = self._geocode_address(request.params['address'])
+            c.people = self._get_people(lat, lon)
+        
         return render('search_form.mako')
+
+    def _geocode_address(self, address):
+       """ convert string address into a lat, long tuple """
+       # move
+       google_api_key = config['google_api_key']
+       geocoder = geopy.geocoders.Google(api_key=google_api_key)
+       addr_str, (lat, lon) = geocoder.geocode(address)
+
+       return lat, lon
+
+    def _get_people(self, lat, lon):
+        """ takes a lat, lon and returns a list of elected officials
+        and  any candidates running for office in districts serving
+        that location """
+        return "%s, %s" % (lat, lon)
+        return []
