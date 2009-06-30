3379ae903fc1fa895acf3496cde7ad3a18da5238
pw@slinktopp <pw@slinktopp>
2009-06-30 16:23:13 -0400

    the admin controller should not create duplicate entries when re-run. Fixed.

--- purplevoter/controllers/admin.py
+++ purplevoter/controllers/admin.py
@@ -12,9 +12,12 @@ log = logging.getLogger(__name__)
 class AdminController(BaseController):
 
     def get_vs_districts(self):
-        """(Re)populate the district and district_meta tables with
-        saved data from the vsdata directory, merged with officials'
-        names from the votesmart api."""
+        """Populate the district and district_meta tables with
+        saved data from the vsdata directory, merged with federal
+        officials' names from the votesmart api.
+
+        XXX this needs to be auth protected
+        """
         import yaml
         from votesmart import votesmart
 
@@ -28,20 +31,37 @@ class AdminController(BaseController):
         state = 'NY'
         for office in states[state]:
             for dist in states[state][office]:
-                print state + ":  " +  offices[office]['level_name'] + ":  " +  offices[office]['name'] + ":  " + dist[0]  
+                district_type = offices[office]['name']
+                district_name = dist[0]
+                level_name = offices[office]['level_name']
+            
+                readable_info = state + ":  " +  level_name + ":  " +  district_type + ":  " + district_name
+                
+                # Avoid duplicate entries. Hooray for meaningless primary keys.
+                
+                exists = meta.Session.query(Districts).filter_by(
+                    state=state, district_type=district_type,
+                    level_name=level_name, district_name=district_name).count()
+                if exists:
+                    print "Already have:", readable_info
+                    continue
+
+                print readable_info
                 district = Districts()
                 district.state = state
-                district.district_type = offices[office]['name']
-                district.level_name = offices[office]['level_name']
-                district.district_name = dist[0]
+                district.district_type = district_type
+                district.level_name = level_name
+                district.district_name = district_name
                 try:
                     official = votesmart.officials.getByDistrict(dist[1])[0]
                     district_meta = DistrictsMeta()
                     district_meta.meta_key = 'official'
                     district_meta.meta_value = official.firstName + " " + official.lastName 
                     district.meta.append(district_meta)
+                    print "VOTESMART: YES"
                 except:
-                    pass
+                    print "VOTESMART: NO for %s" % str((district.state, district.district_type, district.level_name))
+                
                 meta.Session.save(district)
 
         meta.Session.commit()
