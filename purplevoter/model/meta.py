963f46772a9673a9d0c8c7ce92eed92f8f785db4
pw@slinktopp <pw@slinktopp>
2009-07-07 15:03:18 -0400

    Add an SRID, and some updated notes on next steps

--- purplevoter/model/meta.py
+++ purplevoter/model/meta.py
@@ -13,3 +13,11 @@ Session = scoped_session(sessionmaker())
 # Global metadata. If you have multiple databases with overlapping table
 # names, you'll need a metadata for each database
 metadata = MetaData()
+
+# 4326 is the SRID for WGS84
+# (http://en.wikipedia.org/wiki/World_Geodetic_System) ... which seems
+# to be what blockparty uses; stores things as lon/lat internally.
+# Since the main thing we want to query is whether a lon,lat point is
+# within a given district, and we don't care much about distances and
+# suchlike, this is a good choice.
+storage_SRID = 4326
