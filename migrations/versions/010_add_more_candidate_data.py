067b8649b381813ff4822c1ae70a86f3c510059c
pw@slinktopp <pw@slinktopp>
2009-08-18 16:48:21 -0400

    Updated data to latest from TA, final before today's launch.  (Includes Borough President now.)

--- migrations/versions/010_add_more_candidate_data.py
+++ migrations/versions/010_add_more_candidate_data.py
@@ -16,6 +16,33 @@ def find_or_create(session, model, **kw):
         session.save(instance)
     return instance
 
+
+class CaseNormalizingDictReader(csv.DictReader):
+
+    # I wish DictReader had an option for case-insensitive keys.
+    # So here it is.
+
+    class CaseNormalizingDict(dict):
+
+        def __getitem__(self, key):
+            # hacking __setitem__ would probably be smarter. eh.
+            for k in key, key.lower(), key.upper(), key.title():
+                try:
+                    return dict.__getitem__(self, k)
+                except KeyError:
+                    continue
+                raise KeyError(key)
+
+        def get(self, key, default=None):
+            try:
+                return self[key]
+            except KeyError:
+                return default
+
+    def next(self):
+        d = csv.DictReader.next(self)
+        return self.CaseNormalizingDict(d)
+            
     
 def upgrade():
     # Upgrade operations go here. Don't create your own engine; use the engine
@@ -37,6 +64,8 @@ def upgrade():
                   district_type='City', district_format='New York City'),
              dict(path='district_attorney.csv', office='District Attorney',
                   district_type='Borough', district_format=r'%s'),
+             dict(path='borough_president.csv', office='Borough President',
+                  district_type='Borough', district_format=r'%s'),
              dict(path='public_advocate.csv', office='Public Advocate',
                   district_type='City', district_format='New York City'),
              ]
@@ -53,11 +82,12 @@ def _insert_from_file(office, district_type, district_format, path,
 
     path = os.path.join(os.path.dirname(__file__),
                         '..', '..', 'misc_import_data', 
-                        'ta_candidate_info_20090813',
+                        'ta_candidate_info_20090818',
                         path
                         )
     csv_path = os.path.normpath(path)
-    reader = csv.DictReader(open(csv_path, 'r'), skipinitialspace=True)
+    reader = CaseNormalizingDictReader(open(csv_path, 'r'), skipinitialspace=True)
+    # Too bad DictReader doesn't have an option to normalize the case of its args.
 
     district_q = meta.Session.query(Districts)
 
@@ -65,7 +95,11 @@ def _insert_from_file(office, district_type, district_format, path,
         if district_type == 'City Council' and district_format.count:
             district_name = district_format % info['Council District']
         elif district_type == 'Borough':
-            district_name = info['borough']
+            try:
+                district_name = info.get('borough', '').strip()
+            except:
+               district_name = info.get('borough', '').strip()
+                
         else:
             district_name = district_format
 
@@ -76,7 +110,7 @@ def _insert_from_file(office, district_type, district_format, path,
         race = find_or_create(meta.Session, Race,
                               election=election, district=district,
                               office=office)
-        person = find_or_create(meta.Session, People, fullname=info['fullname'])
+        person = find_or_create(meta.Session, People, fullname=info['fullname'].strip())
 
         race.candidates.append(person)
 
@@ -86,7 +120,7 @@ def _insert_from_file(office, district_type, district_format, path,
         # If TA's app was using UUIDs, I could just use their IDs but
         # that's not happening now.
 
-        transaltid = info.get('NID', '').strip()
+        transaltid = (info.get('NID') or '').strip()
         if transaltid:
             ta_id = find_or_create(meta.Session, PeopleMeta,
                                    meta_key='transaltid', meta_value=transaltid,
